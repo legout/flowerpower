0e2b1d2e085389b08f63a26454ba3b1700030548
legout <ligno.blades@gmail.com>
2024-06-01 20:54:43 +0200

    update

--- src/flowerpower/main.py
+++ src/flowerpower/main.py
@@ -7,14 +7,14 @@ def init(conf_path: str = "conf", pipelines_path: str = "pipelines"):
     os.makedirs(conf_path, exist_ok=True)
     os.makedirs(pipelines_path, exist_ok=True)
 
-    write({"path": pipelines_path, "run": {}, "params": {}}, "pipelines", conf_path)
+    write({"path": pipelines_path, "run": None, "params": None}, "pipelines", conf_path)
     write(
         {
             "username": None,
             "api_url": "http://localhost:8241",
             "ui_url": "http://localhost:8242",
             "api_key": None,
-            "pipeline": {},
+            "pipeline": None,
         },
         "tracker",
         conf_path,
@@ -23,7 +23,7 @@ def init(conf_path: str = "conf", pipelines_path: str = "pipelines"):
         {
             "data_path": {"type": "memory"},
             "event_broker": {"type": "local"},
-            "pipeline": {},
+            "pipeline": None,
         },
         "scheduler",
         conf_path,
