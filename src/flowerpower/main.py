992d6c6e4aa01a82891a3d99c8332eaeeffb6ed8
legout <ligno.blades@gmail.com>
2024-06-01 20:31:43 +0200

    added cli

--- src/flowerpower/main.py
+++ src/flowerpower/main.py
@@ -1,2 +1,10 @@
-def init():
-    
+import os
+from .cfg import write, TRACKER_TEMPLATE, PIPELINE_TEMPLATE, SCHEDULER_TEMPLATE
+
+def init(conf_path: str = "conf", pipelines_path: str = "pipelines"):
+    os.makedirs(conf_path, exist_ok=True)
+    os.makedirs(pipelines_path, exist_ok=True)
+
+    write(PIPELINE_TEMPLATE, "pipelines", conf_path)
+    write(TRACKER_TEMPLATE, "tracker", conf_path)
+    write(SCHEDULER_TEMPLATE, "scheduler", conf_path)
