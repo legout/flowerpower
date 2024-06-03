aa858bd5776283ed3ad22570c132f44bc2b3543c
Volker Lorrmann <z0043ddz@macvgfd7qj3n6.speedport.ip>
2024-06-04 00:52:53 +0200

    updated README

--- examples/pipelines/my_flow.py
+++ examples/pipelines/my_flow.py
@@ -1,3 +1,48 @@
-# FlowerPower pipeline my_flow.py
-# Created at 2024-06-01 14:34:17.032441
+import pandas as pd
+from hamilton.function_modifiers import parameterize
+from flowerpower.cfg import load_pipeline_cfg
 
+PARAMS = load_pipeline_cfg(ht_values=True).params.my_flow
+
+
+def spend() -> pd.Series:
+    """Returns a series of spend data."""
+    return pd.Series([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+
+
+def signups() -> pd.Series:
+    """Returns a series of signups data."""
+    return pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+
+@parameterize(avg_x_wk_spend=dict(rolling=PARAMS.avg_x_wk_spend.rolling))
+def avg_x_wk_spend(spend: pd.Series, rolling: int) -> pd.Series:
+    """Rolling x week average spend."""
+    return spend.rolling(rolling).mean()
+
+
+def spend_per_signup(spend: pd.Series, signups: pd.Series) -> pd.Series:
+    """The cost per signup in relation to spend."""
+    return spend / signups
+
+
+def spend_mean(spend: pd.Series) -> float:
+    """Shows function creating a scalar. In this case it computes the mean of the entire column."""
+    return spend.mean()
+
+
+def spend_zero_mean(spend: pd.Series, spend_mean: float) -> pd.Series:
+    """Shows function that takes a scalar. In this case to zero mean spend."""
+    return spend - spend_mean
+
+
+def spend_std_dev(spend: pd.Series) -> float:
+    """Function that computes the standard deviation of the spend column."""
+    return spend.std()
+
+
+def spend_zero_mean_unit_variance(
+    spend_zero_mean: pd.Series, spend_std_dev: float
+) -> pd.Series:
+    """Function showing one way to make spend have zero mean and unit variance."""
+    return spend_zero_mean / spend_std_dev
--- examples/pipelines/my_flow2.py
+++
@@ -1,3 +0,0 @@
-# FlowerPower pipeline my_flow2.py
-# Created at 2024-06-01 14:34:33.275991
-
