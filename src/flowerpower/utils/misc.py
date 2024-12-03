20e78066dc78af194fb64725198f02b3851aaeb6
Volker L. <volker.lorrmann@gmail.com>
2024-12-03 22:48:41 +0100

    Refactor import statements and conditionally import pyarrow and joblib; enhance convert_large_types_to_standard and run_parallel functions for improved flexibility and error handling.

--- src/flowerpower/utils/misc.py
+++ src/flowerpower/utils/misc.py
@@ -1,79 +1,81 @@
 import os
+import importlib
 
-import pyarrow as pa
 import tqdm
-from joblib import Parallel, delayed
 import tempfile
 import subprocess
 import time
 
+if importlib.util.find_spec("pyarrow"):
+    import pyarrow as pa
+    def convert_large_types_to_standard(schema: pa.Schema) -> pa.Schema:
+        # Define mapping of large types to standard types
+        type_mapping = {
+            pa.large_string(): pa.string(),
+            pa.large_binary(): pa.binary(),
+            pa.large_list(pa.null()): pa.list_(pa.null()),
+        }
 
-def convert_large_types_to_standard(schema: pa.Schema) -> pa.Schema:
-    # Define mapping of large types to standard types
-    type_mapping = {
-        pa.large_string(): pa.string(),
-        pa.large_binary(): pa.binary(),
-        pa.large_list(pa.null()): pa.list_(pa.null()),
-    }
+        # Convert fields
+        new_fields = []
+        for field in schema:
+            field_type = field.type
+            # Check if type exists in mapping
+            if field_type in type_mapping:
+                new_field = pa.field(
+                    name=field.name,
+                    type=type_mapping[field_type],
+                    nullable=field.nullable,
+                    metadata=field.metadata,
+                )
+                new_fields.append(new_field)
+            # Handle large lists with nested types
+            elif isinstance(field_type, pa.LargeListType):
+                new_field = pa.field(
+                    name=field.name,
+                    type=pa.list_(field_type.value_type),
+                    nullable=field.nullable,
+                    metadata=field.metadata,
+                )
+                new_fields.append(new_field)
+            else:
+                new_fields.append(field)
 
-    # Convert fields
-    new_fields = []
-    for field in schema:
-        field_type = field.type
-        # Check if type exists in mapping
-        if field_type in type_mapping:
-            new_field = pa.field(
-                name=field.name,
-                type=type_mapping[field_type],
-                nullable=field.nullable,
-                metadata=field.metadata,
+        return pa.schema(new_fields)
+
+if importlib.util.find_spec("joblib"):
+    from joblib import Parallel, delayed
+
+    def run_parallel(
+        func: callable,
+        func_params: list[any],
+        *args,
+        n_jobs: int = -1,
+        backend: str = "threading",
+        verbose: bool = True,
+        **kwargs,
+    ) -> list[any]:
+        """Runs a function for a list of parameters in parallel.
+
+        Args:
+            func (Callable): function to run in Parallelallel.
+            func_params (list[any]): parameters for the function
+            n_jobs (int, optional): Number of joblib workers. Defaults to -1.
+            backend (str, optional): joblib backend. Valid options are
+            `loky`,`threading`, `mutliprocessing` or `sequential`.  Defaults to "threading".
+
+        Returns:
+            list[any]: Function output.
+        """
+        if verbose:
+            return Parallel(n_jobs=n_jobs, backend=backend)(
+                delayed(func)(fp, *args, **kwargs) for fp in tqdm.tqdm(func_params)
             )
-            new_fields.append(new_field)
-        # Handle large lists with nested types
-        elif isinstance(field_type, pa.LargeListType):
-            new_field = pa.field(
-                name=field.name,
-                type=pa.list_(field_type.value_type),
-                nullable=field.nullable,
-                metadata=field.metadata,
-            )
-            new_fields.append(new_field)
+
         else:
-            new_fields.append(field)
-
-    return pa.schema(new_fields)
-
-
-def run_parallel(
-    func: callable,
-    func_params: list[any],
-    *args,
-    n_jobs: int = -1,
-    backend: str = "threading",
-    verbose: bool = True,
-    **kwargs,
-) -> list[any]:
-    """Runs a function for a list of parameters in parallel.
-
-    Args:
-        func (Callable): function to run in Parallelallel.
-        func_params (list[any]): parameters for the function
-        n_jobs (int, optional): Number of joblib workers. Defaults to -1.
-        backend (str, optional): joblib backend. Valid options are
-        `loky`,`threading`, `mutliprocessing` or `sequential`.  Defaults to "threading".
-
-    Returns:
-        list[any]: Function output.
-    """
-    if verbose:
-        return Parallel(n_jobs=n_jobs, backend=backend)(
-            delayed(func)(fp, *args, **kwargs) for fp in tqdm.tqdm(func_params)
-        )
-
-    else:
-        return Parallel(n_jobs=n_jobs, backend=backend)(
-            delayed(func)(fp, *args, **kwargs) for fp in func_params
-        )
+            return Parallel(n_jobs=n_jobs, backend=backend)(
+                delayed(func)(fp, *args, **kwargs) for fp in func_params
+            )
 
 
 def get_partitions_from_path(
