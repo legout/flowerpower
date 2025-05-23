599f32683fa51e605d419cd3f5e73a8c68315542
legout <ligno.blades@gmail.com>
2025-05-23 19:29:59 +0200

    Add basic MQTT pipeline configuration and implementation
    - Created a new YAML configuration file for the basic MQTT pipeline, defining adapters, parameters, and execution settings.
    - Implemented the basic_mqtt.py script to handle MQTT messages, convert payloads to Polars DataFrames, and log DataFrame details.

---
+++ examples/hello-world/pipelines/basic_mqtt.py
@@ -0,0 +1,46 @@
+# FlowerPower pipeline basic_mqtt.py
+# Created on 2025-05-21 13:30:33
+
+####################################################################################################
+# Import necessary libraries
+# NOTE: Remove or comment out imports that are not used in the pipeline
+
+#from hamilton.function_modifiers import parameterize, dataloader, datasaver
+#from hamilton.htypes import Parallelizable, Collect
+import orjson
+from pathlib import Path
+from loguru import logger
+from flowerpower.cfg import Config
+import polars as pl
+####################################################################################################
+# Load pipeline parameters. Do not modify this section.
+
+PARAMS = Config.load(
+    Path(__file__).parents[1], pipeline_name="basic_mqtt"
+).pipeline.h_params
+
+
+####################################################################################################
+# Helper functions.
+# This functions have to start with an underscore (_).
+
+
+####################################################################################################
+# Pipeline functions
+
+def data(payload: bytes, topic: str) -> dict:
+    """Loads the payload from the MQTT message."""
+    logger.info(f"Received message on topic {topic}")
+    return orjson.loads(payload)
+
+
+def to_df(data: dict) -> pl.DataFrame:
+    """Converts the payload to a Polars DataFrame."""
+    df = pl.DataFrame(data)
+    return df
+
+
+def print_df(to_df: pl.DataFrame) -> None:
+    """Prints the DataFrame shape and head."""
+    logger.info(f"Dataframe shape: {to_df.shape}")
+    logger.info(f"Dataframe head:{to_df.head()}")
