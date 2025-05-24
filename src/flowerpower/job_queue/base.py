5cfc48e7162cdb10ef28c4336d540422abad921d
legout <ligno.blades@gmail.com>
2025-05-24 23:36:58 +0200

    chore: Update version to 0.10.7.4; fix incorrect attribute reference for filesystem mapper in job queue and pipeline manager

--- src/flowerpower/job_queue/base.py
+++ src/flowerpower/job_queue/base.py
@@ -387,7 +387,7 @@ class BaseJobQueueManager:
         """
         if self._fs.is_cache_fs:
             self._fs.sync_cache()
-            project_path = self._fs_mapper.directory
+            project_path = self._fs._mapper.directory
             modules_path = posixpath.join(project_path, self._pipelines_dir)
 
         else:
