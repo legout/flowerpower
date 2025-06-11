b7339f4f361747ae6bc081135ff193591bc6751d
legout <ligno.blades@gmail.com>
2025-06-11 11:59:32 +0200

    fix: improve comment formatting in BaseFileIO

--- src/flowerpower/plugins/io/base.py
+++ src/flowerpower/plugins/io/base.py
@@ -85,7 +85,7 @@ class BaseFileIO(msgspec.Struct, gc=False):
     def __post_init__(self):
         # self._base_path = self.path if isinstance(self.path, str) else os.path.commonpath(self.path)
 
-        #if self.fs is None:
+        # if self.fs is None:
         self.fs = get_filesystem(
             path=self._base_path,
             storage_options=self.storage_options,
