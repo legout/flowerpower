6c7089cad944caa63be686eefff05f6cba54c750
legout <ligno.blades@gmail.com>
2025-08-30 10:15:32 +0000

    feat(mqtt): deprecate mqtt module and remove related files

--- src/flowerpower/plugins/io/__init__.py
+++ src/flowerpower/plugins/io/__init__.py
@@ -1,7 +1,7 @@
 import warnings
 
 warnings.warn(
-    "The flowerpower.plugins._io module is deprecated. "
+    "The flowerpower.plugins.io module is deprecated. "
     "Please use 'flowerpower-io' instead. Install it with 'pip install flowerpower-io'.",
     DeprecationWarning,
     stacklevel=2,
